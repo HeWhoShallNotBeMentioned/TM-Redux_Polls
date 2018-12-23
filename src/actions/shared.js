30375881883c7314c039f6f74c3695d48474c54a
Chris Underwood <cunderwoodmn@gmail.com>
2018-12-23 13:21:28 -0800

    create new loading widget/page using npm package react-redux-loading for when the app page is loading new data.

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,16 +2,19 @@ import {getInitialData} from '../utils/api';
 import {receiveUsers} from './users';
 import {receivePolls} from './polls';
 import {setAuthedUser} from './authedUser'
+import { showLoading, hideLoading } from 'react-redux-loading'
 
 const AUTHED_ID = 'sarah_edo';
 
 export function handleInitialData () {
   return async (dispatch) => {
+    dispatch(showLoading());
     const response = await getInitialData();
     const polls = response.polls;
     const users = response.users;
     dispatch(receivePolls(polls));
     dispatch(receiveUsers(users));
     dispatch(setAuthedUser(AUTHED_ID));
+    dispatch(hideLoading());
   }
 }
--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { handleInitialData } from '../actions/shared';
 import Dashboard from './Dashboard';
+import LoadingBar from 'react-redux-loading';
 
 class App extends Component {
   componentDidMount () {
@@ -12,7 +13,7 @@ class App extends Component {
     return (
       <div>
       {this.props.loading === true
-      ? ''
+      ? <LoadingBar style={{ backgroundColor: 'blue', height: '30px' }} updateTime={100} maxProgress={95} progressIncrease={30} />
       : <Dashboard />}
         
       </div>
