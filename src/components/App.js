377145ce9e404faf2fb4f77666a163f85c8075ec
Chris Underwood <cunderwoodmn@gmail.com>
2018-12-23 16:34:21 -0800

    create AddPoll component and put the UI for it

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import { handleInitialData } from '../actions/shared';
 import Dashboard from './Dashboard';
 import LoadingBar from 'react-redux-loading';
 import Leaderboard from './Leaderboard';
+import AddPoll from './AddPoll';
 
 class App extends Component {
   componentDidMount () {
@@ -15,7 +16,7 @@ class App extends Component {
       <div>
       {this.props.loading === true
       ? <LoadingBar style={{ backgroundColor: 'RoyalBlue', height: '30px' }} updateTime={100} maxProgress={95} progressIncrease={30} />
-      : <Leaderboard />}
+      : <AddPoll />}
         
       </div>
     )
